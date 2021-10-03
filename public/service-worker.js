30e17c49fa288c82b7fcf0f826192637bd3912c6
BA1bers <brent.albers@yahoo.com>
2021-10-02 23:13:39 -0700

    updated service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'transaction-cache'
+const DATA_CACHE_NAME = ''
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/favicon.ico',
+    '/manifest.json',
+    '/app.js',
+    '/assets/css/style.css',
+    '/assets/images/icons/icon-72x72.png',
+    '/assets/images/icons/icon-96x96.png',
+    '/assets/images/icons/icon-128x128.png',
+    '/assets/images/icons/icon-144x144.png',
+    '/assets/images/icons/icon-152x152.png',
+    '/assets/images/icons/icon-192x192.png',
+    '/assets/images/icons/icon-384x384.png',
+    '/assets/images/icons/icon-512x512.png',
+    '/js/idb.js'
+];
